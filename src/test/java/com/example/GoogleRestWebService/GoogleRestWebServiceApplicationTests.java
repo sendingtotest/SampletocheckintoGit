74f073f31fcaae61cfca8c0b68132b3c24ed5d70
sendingtotest <57683327+sendingtotest@users.noreply.github.com>
2020-06-22 17:12:03 -0400

    Update GoogleRestWebServiceApplicationTests.java

--- src/test/java/com/example/GoogleRestWebService/GoogleRestWebServiceApplicationTests.java
+++ src/test/java/com/example/GoogleRestWebService/GoogleRestWebServiceApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.GoogleRestWebService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//this is a test class
+//this is a test class ......
 @SpringBootTest
 class GoogleRestWebServiceApplicationTests {
 
